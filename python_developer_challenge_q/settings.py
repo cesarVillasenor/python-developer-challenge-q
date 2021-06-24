ef4e13f081694e040cb8b802683ba698e10a9f9c
Cesar <cesar.villasenor.g@gmail.com>
2021-06-23 21:50:12 -0700

    (v0.1) Project settings  updated to integrate drf and dev requirements created.

--- python_developer_challenge_q/settings.py
+++ python_developer_challenge_q/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.user',
 ]
 
@@ -70,6 +71,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'python_developer_challenge_q.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
